f35666c7779b20b5e68bcd8a28130362f264f472
mdalamaladm <muhammaddzakyalam@gmail.com>
2026-08-04 09:04:46 +0300

    feat(cpp/QT/Widget): add statusbar, toolbar, central area, and basic display widget

--- cpp/QT/Widgets/main.cpp
+++ cpp/QT/Widgets/main.cpp
@@ -87,6 +87,9 @@ public:
         setWindowTitle("QT6 Widgets Showcase");
 
         buildMenuBar();
+        buildStatusBar();
+        buildToolBar();
+        buildCentralArea();
 
         resize(900, 700);
     }
@@ -104,9 +107,54 @@ private:
         QAction *colorAction = fileMenu->addAction("Pick Color...");
         connect(colorAction, &QAction::triggered, this, &ShowcaseWindow::onPickColor);
 
+        QAction *fontAction = fileMenu->addAction("Pick Font...");
+        connect(fontAction, &QAction::triggered, this, &ShowcaseWindow::onPickFont);
+
+        QAction *inputAction = fileMenu->addAction("Text Input...");
+        connect(inputAction, &QAction::triggered, this, &ShowcaseWindow::onTextInput);
+
         fileMenu->addSeparator();
         QAction *exitAction = fileMenu->addAction("Exit");
         connect(exitAction, &QAction::triggered, this, &QWidget::close);
+    }
+
+    void buildToolBar()
+    {
+        QToolBar *toolBar = addToolBar("Main Toolbar");
+        toolBar->setMovable(true);
+        toolBar->setFloatable(true);
+        toolBar->setIconSize(QSize(12, 12));
+        toolBar->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+
+        QAction *msgAction = toolBar->addAction(
+            style()->standardIcon(QStyle::SP_MessageBoxInformation),
+            "Show Message"
+        );
+
+        connect(msgAction, &QAction::triggered, this, &ShowcaseWindow::onShowMessage);
+    }
+
+    void buildStatusBar()
+    {
+        QStatusBar *status = statusBar();
+        status->setSizeGripEnabled(true);
+        status->showMessage("Ready", 5000);
+    }
+
+    void buildCentralArea()
+    {
+        auto *scrollArea = new QScrollArea;
+        scrollArea->setWidgetResizable(true);
+        scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+        scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+        auto *content = new QWidget;
+        auto *mainLayout = new QVBoxLayout(content);
+
+        mainLayout->addWidget(buildDisplayGroup());
+
+        scrollArea->setWidget(content);
+        setCentralWidget(scrollArea);
     }
 
     void onOpenFile()
@@ -130,6 +178,66 @@ private:
         if (color.isValid()) {
             statusBar()->showMessage("Color chosen: " + color.name(), 4000);
         }
+    }
+
+    void onPickFont()
+    {
+        bool ok = false;
+        QFont font = QFontDialog::getFont(&ok, QFont("Arial", 12), this, "Pick a Font");
+
+        if (ok) {
+            statusBar()->showMessage("Font chosen: " + font.family(), 4000);
+        }
+    }
+
+    void onTextInput()
+    {
+        bool ok = false;
+        QString text = QInputDialog::getText(this, "Input Dialog", "Enter your name:", QLineEdit::Normal, "", &ok);
+
+        if (ok && !text.isEmpty()) {
+            statusBar()->showMessage("You entered: " + text, 4000);
+        }
+    }
+
+    void onShowMessage()
+    {
+        QMessageBox msgBox(this);
+        msgBox.setText("This is a QMessageBox.");
+        msgBox.setStyleSheet("QMessageBox { min-width: 400px; min-height: 200px; }");
+        msgBox.setIcon(QMessageBox::Information);
+        msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
+
+    QGroupBox *buildDisplayGroup()
+    {
+        auto *group = new QGroupBox("Basic Display Widgets");
+        auto *layout = new QHBoxLayout(group);
+
+        auto *label = new QLabel("<b>Hello Qt6!</b>");
+        label->setAlignment(Qt::AlignCenter);
+        label->setWordWrap(true);
+        label->setToolTip("This is a QLabel");
+        layout->addWidget(wrap("QLabel", label));
+
+        auto *lcd = new QLCDNumber;
+        lcd->setDigitCount(5);
+        lcd->setMode(QLCDNumber::Dec);
+        lcd->setSegmentStyle(QLCDNumber::Flat);
+        lcd->display(12345);
+        layout->addWidget(wrap("QLCDNumber", lcd));
+
+        auto *progress = new QProgressBar;
+        progress->setMinimum(0);
+        progress->setMaximum(100);
+        progress->setValue(65);
+        progress->setTextVisible(true);
+        progress->setFormat("%p% complete");
+        layout->addWidget(wrap("QProgressBar", progress));
+
+        return group;
     }
 };
 
